3fe03e3a4841364d5cf0106a5e0fa0c38c784b30
vivacazador <119748397+vivacazador@users.noreply.github.com>
2024-12-23 21:45:47 -0800

    get things running locally

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'], // Add the external image domain here
+  },
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
